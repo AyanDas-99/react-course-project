c8451d9b8fbd805c71c22ac291719d5e226434c3
Ayan Das <ayandas9531@gmail.com>
2022-12-29 20:13:25 +0530

    sends to db

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Post } from './pages/create-post/Post';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-
-   </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/createpost' element={<Post />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
